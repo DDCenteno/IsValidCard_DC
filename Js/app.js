326ad3d6a64da16e5c3e235ff31da3a1a73fa22d
DDCenteno <danielacentenoramos@gmail.com>
2017-10-10 09:43:54 -0500

    espero entre ewe

---
+++ Js/app.js
@@ -0,0 +1,6 @@
+numberInside= [prompt("Ingrese su numero de tarjeta de credito:")];
+isValidCard(numberInside);
+function isValidCard (number){
+    numberParse = number.parseInt;// se vuelve entero los digitos que ingresaron del prompt.
+    
+}
